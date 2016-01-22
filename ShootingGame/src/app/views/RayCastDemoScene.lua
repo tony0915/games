64de9238214410ff168da2382f1178c7e7e7ff59
tony0915 <taozeng0915>
2016-01-22 18:21:39 +0800

    changed the layer.
    changed the layer.

--- ShootingGame/src/app/views/RayCastDemoScene.lua
+++ ShootingGame/src/app/views/RayCastDemoScene.lua
@@ -4,17 +4,24 @@ local VisibleRect = import("..utils.VisibleRect")
 local MATERIAL_DEFAULT = cc.PhysicsMaterial(0.1, 0.5, 0.5)
 local STATIC_COLOR = cc.c4f(1.0, 0.0, 0.0, 1.0)
 
-local function makePolygon()
+local function makePolygon(points)
+  assert(points and #points>2, "the number of points mustbe greater than 2!")
+
 	local drawNode = cc.DrawNode:create()
-
-    local points = {cc.p(-100, -100), cc.p(-100, 100), cc.p(100, 200), cc.p(100, -90)}
         
-    drawNode:drawPolygon(points, table.getn(points), cc.c4f(1,1,1,0.5), 1, cc.c4f(0,0,0,1))
+  drawNode:drawPolygon(points, table.getn(points), cc.c4f(1,1,1,0.5), 1, cc.c4f(0,0,0,1))
 
-    local body = cc.PhysicsBody:createPolygon(points, MATERIAL_DEFAULT)
-    drawNode:setPhysicsBody(body)
+  local body = cc.PhysicsBody:createPolygon(points, MATERIAL_DEFAULT)
+  drawNode:setPhysicsBody(body)
 
 	return drawNode
+end
+
+local function makeCircle()
+    local drawNode = cc.DrawNode:create()
+    drawNode:drawDot(cc.p(0, 0), 2, cc.c4f(1.0, 1.0, 1.0, 1.0))
+
+    return drawNode
 end
 
 local function makeBall(layer, point, radius, material)
@@ -91,48 +98,40 @@ end
 local function PhysicsDemoRayCast()
     local layer = cc.Layer:create()
 
+    local drawNodesLayer = cc.Layer:create()
+    layer:addChild(drawNodesLayer)
+
     local function onEnter()
 
-    	local polygon1 = makePolygon()
-    	polygon1:setPosition(cc.p(300, 300))
-    	layer:addChild(polygon1)
+        local center = makeCircle()
+        center:setPosition(cc.p(0, 0))
+        layer:addChild(center)
 
-       local function onTouchEnded(touch, event)
-	         local location = touch:getLocation()
-	  
-	         -- local r = math.random(3)
-	         -- if r ==1 then
-	         --     layer:addChild(makeBall(layer, location, 5 + math.random()*10))
-	         -- elseif r == 2 then
-	         --     layer:addChild(makeBox(location, cc.size(10 + math.random()*15, 10 + math.random()*15)))
-	         -- elseif r == 3 then
-	         --     layer:addChild(makeTriangle(location, cc.size(10 + math.random()*20, 10 + math.random()*20)))
-	         -- end
-       end
+    	  local polygon1 = makePolygon({cc.p(0, 0), cc.p(0, 100), cc.p(100, 100), cc.p(100, 0)})
+    	  polygon1:setPosition(cc.p(300, 300))
+    	  layer:addChild(polygon1)
+
+        local polygon2 = makePolygon({cc.p(0, 100), cc.p(100, 100), cc.p(150, 0)})
+        polygon2:setPosition(cc.p(600, 100))
+        layer:addChild(polygon2)
        
-       local touchListener = cc.EventListenerTouchOneByOne:create()
-       touchListener:registerScriptHandler(function() return true end, cc.Handler.EVENT_TOUCH_BEGAN)
-       touchListener:registerScriptHandler(onTouchEnded, cc.Handler.EVENT_TOUCH_ENDED)
-       local eventDispatcher = layer:getEventDispatcher()
-       eventDispatcher:addEventListenerWithSceneGraphPriority(touchListener, layer)
+        cc.Director:getInstance():getRunningScene():getPhysicsWorld():setGravity(cc.p(0,0))
        
-       cc.Director:getInstance():getRunningScene():getPhysicsWorld():setGravity(cc.p(0,0))
-       
-       local node = cc.DrawNode:create()
-       node:setPhysicsBody(cc.PhysicsBody:createEdgeSegment(cc.p(VisibleRect:leftBottom().x, 
+        local node = cc.DrawNode:create()
+        node:setPhysicsBody(cc.PhysicsBody:createEdgeSegment(cc.p(VisibleRect:leftBottom().x, 
                                                                  VisibleRect:leftBottom().y + 50), 
                                                             cc.p(VisibleRect:rightBottom().x, 
                                                                  VisibleRect:rightBottom().y + 50)))
-       node:drawSegment(cc.p(VisibleRect:leftBottom().x, VisibleRect:leftBottom().y + 50), 
+        node:drawSegment(cc.p(VisibleRect:leftBottom().x, VisibleRect:leftBottom().y + 50), 
                         cc.p(VisibleRect:rightBottom().x, VisibleRect:rightBottom().y + 50), 
                         1, 
                         STATIC_COLOR)
-       layer:addChild(node)
+        layer:addChild(node)
 
-       local mode = 0
-       cc.MenuItemFont:setFontSize(18)
-       local item = cc.MenuItemFont:create("Toogle debugChange Mode(any)")
-       local function changeModeCallback(sender)
+        local mode = 0
+        cc.MenuItemFont:setFontSize(18)
+        local item = cc.MenuItemFont:create("Toogle debugChange Mode(any)")
+        local function changeModeCallback(sender)
 	         mode = (mode + 1) % 3
 	  
 	         if mode == 0 then
@@ -142,79 +141,70 @@ local function PhysicsDemoRayCast()
 	         elseif mode == 2 then
 	         item:setString("Change Mode(multiple)")
 	         end
-       end
+        end
        
-       item:registerScriptTapHandler(changeModeCallback)
+        item:registerScriptTapHandler(changeModeCallback)
        
-       local menu = cc.Menu:create(item)
-       layer:addChild(menu)
-       menu:setPosition(cc.p(VisibleRect:left().x+100, VisibleRect:top().y-10))
+        local menu = cc.Menu:create(item)
+        layer:addChild(menu)
+        menu:setPosition(cc.p(VisibleRect:left().x + 100, VisibleRect:top().y - 10))
 
-       local angle = 0
-       local drawNode = nil
-       local function update(delta)
-	         local L = 250.0
-	         local point1 = VisibleRect:center()
-	         local d = cc.p(L * math.cos(angle), L * math.sin(angle))
-	         local point2 = cc.p(point1.x + d.x, point1.y + d.y)
-    
-          if drawNode then layer:removeChild(drawNode) end
-          drawNode = cc.DrawNode:create()
-          if mode == 0 then
-	            local point3 = cc.p(point2.x, point2.y)
-	            local function func(world, info)
-		              point3 = info.contact
-		              return false
-	            end
+        local angle = 0
+        local function update(delta)
+	         local L = 500.0
+	         local point1 = cc.p(center:getPositionX(), center:getPositionY()) --VisibleRect:center()
 
-              cc.Director:getInstance():getRunningScene():getPhysicsWorld():rayCast(func, point1, point2)
-              drawNode:drawSegment(point1, point3, 1, STATIC_COLOR)
-            
-              if point2.x ~= point3.x or point2.y ~= point3.y then
-	               drawNode:drawDot(point3, 2, cc.c4f(1.0, 1.0, 1.0, 1.0))
-              end
-              layer:addChild(drawNode)
-	        elseif mode == 1 then
-	            local point3 = cc.p(point2.x, point2.y)
-              local friction = 1.0
-              local function func(world, info)
-                  if friction > info.fraction then
-                      point3 = info.contact
-                      friction = info.fraction
-		              end
-                  return true
-              end
-            
-              cc.Director:getInstance():getRunningScene():getPhysicsWorld():rayCast(func, point1, point2)
-              drawNode:drawSegment(point1, point3, 1, STATIC_COLOR)
-            
-              if point2.x ~= point3.x or point2.y ~= point3.y then
-                  drawNode:drawDot(point3, 2, cc.c4f(1.0, 1.0, 1.0, 1.0))
-              end
-              layer:addChild(drawNode)
-          elseif mode == 2 then
-	            local points = {}
-            
-              local function func(world, info)
-                  points[#points + 1] = info.contact
-                  return true
-              end
-            
-              cc.Director:getInstance():getRunningScene():getPhysicsWorld():rayCast(func, point1, point2)
-              drawNode:drawSegment(point1, point2, 1, STATIC_COLOR)
-            
-              for _, p in ipairs(points) do
-                  drawNode:drawDot(p, 2, cc.c4f(1.0, 1.0, 1.0, 1.0))
-              end
-            
-              layer:addChild(drawNode)
+           drawNodesLayer:removeAllChildren()
+          --if drawNode then layer:removeChild(drawNode) end
+          for i=1,100 do
+            local drawNode = cc.DrawNode:create()
+            if mode == 0 then
+                local d = cc.p(L * math.cos(angle), L * math.sin(angle))
+                local point2 = cc.p(point1.x + d.x, point1.y + d.y)
+
+                local point3 = cc.p(point2.x, point2.y)
+                local function func(world, info)
+                    point3 = info.contact
+                    return false
+                end
+
+                cc.Director:getInstance():getRunningScene():getPhysicsWorld():rayCast(func, point1, point2)
+                drawNode:drawSegment(point1, point3, 1, STATIC_COLOR)
+              
+                if point2.x ~= point3.x or point2.y ~= point3.y then
+                   drawNode:drawDot(point3, 2, cc.c4f(1.0, 1.0, 1.0, 1.0))
+                end
+                drawNodesLayer:addChild(drawNode)
+                print("drawNode called!!!!!!!!!!!!!!!!!!!!!")
+            end
+
+            angle = 2 * math.pi * i/100
           end
-    
-         angle = angle + 0.25 * math.pi / 180.0
+        end
 
+      local function onTouchEnded(touch, event)
+        local location = touch:getLocation()
+        print("location: ", location.x, location.y)
+        center:setPosition(cc.p(location.x, location.y))
+        update()
       end
 
-       layer:scheduleUpdateWithPriorityLua(update, 0)
+      local function onTouchMoved(touch, event)
+        print("touch moved!!!!!!!!!!!!!!")
+        local location = touch:getLocation()
+        center:setPosition(cc.p(location.x, location.y))
+
+        update()
+      end
+
+      local touchListener = cc.EventListenerTouchOneByOne:create()
+      touchListener:registerScriptHandler(function() return true end, cc.Handler.EVENT_TOUCH_BEGAN)
+      touchListener:registerScriptHandler(onTouchMoved, cc.Handler.EVENT_TOUCH_MOVED)
+      touchListener:registerScriptHandler(onTouchEnded, cc.Handler.EVENT_TOUCH_ENDED)
+      local eventDispatcher = layer:getEventDispatcher()
+      eventDispatcher:addEventListenerWithSceneGraphPriority(touchListener, layer)
+
+      --layer:scheduleUpdateWithPriorityLua(update, 0)
     end
 
    	local function onNodeEvent(event)
